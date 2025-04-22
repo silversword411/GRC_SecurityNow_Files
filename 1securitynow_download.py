37d0ef4237ab806707f3a678fbaa4b52da608a7b
silversword411 <github.com@datmail.com>
2025-04-22 18:17:49 -0400

    Updating to 1021

--- 1securitynow_download.py
+++ 1securitynow_download.py
@@ -2,8 +2,8 @@ import os
 import requests
 
 # Define the range of file numbers
-start_num = 1000
-end_num = 1013
+start_num = 1012
+end_num = 1021
 
 # Define the URL template
 url_template = "https://www.grc.com/sn/sn-{:03d}.txt"
